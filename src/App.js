5c16514e31b9c7cf11a0137856ba5f748154aaed
Carlos Martorini <c7.silveira@gmail.com>
2021-05-06 19:29:34 -0300

    feat: add card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Card from './components/Card';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <h2>🚀 Trending Jobs</h2>
+      <div className='cardList'>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </div>
   );
 }
