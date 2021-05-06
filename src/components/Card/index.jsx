379e2cfbf49f099de2d30154ab5f317fafcaf47a
Carlos Martorini <c7.silveira@gmail.com>
2021-05-06 20:29:30 -0300

    style: adjust some styles and width screen

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -11,7 +11,7 @@ class Card extends Component {
         const { job } = this.props;
         return(
             <div className='card'>
-                <img src={job.company_logo} alt='company-logo'/>
+                <img src={job.company_logo ? job.company_logo : 'https://www.allianceplast.com/wp-content/uploads/2017/11/no-image.png'} alt='company-logo'/>
                 <h3>{job.company.length > 15 ? `${job.company.substring(0, 15)}...` : job.company}</h3>
                 <p>{job.title.length > 15 ? `${job.title.substring(0, 15)}...` : job.title}</p>
                 <span>{job.location}</span>
